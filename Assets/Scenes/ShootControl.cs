ebde7111e335153120a0b57747a99750111cdc0c
Katawoot Keawnil <Kxtawoot2003@Gmail.com>
2025-05-06 15:34:49 +0700

    Reload

--- Assets/Scenes/ShootControl.cs
+++ Assets/Scenes/ShootControl.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections;
 
 public class ShootControl : MonoBehaviour
 {
@@ -8,20 +9,54 @@ public class ShootControl : MonoBehaviour
 
     [Header("Settings")]
     [SerializeField] private float bulletSpeed = 10f;
-    [SerializeField] private float bulletLifeTime = 3f; 
+    [SerializeField] private float bulletLifeTime = 3f;
+    [SerializeField] private int maxAmmo = 5; // จำนวนกระสุนสูงสุดที่สามารถยิงได้
+    [SerializeField] private float reloadTime = 2f; // เวลาที่ใช้ในการรีโหลด
+
+    private int currentAmmo;
+    private bool isReloading = false;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        currentAmmo = maxAmmo;
         UIManager.OnUIShootButton += Shoot;
     }
 
     // Update is called once per frame
+    void Update()
+    {
+        // รีโหลดอัตโนมัติหากกระสุนหมด
+        if (currentAmmo == 0 && !isReloading)
+        {
+            StartCoroutine(Reload());
+        }
+    }
+
+    // ฟังก์ชันยิงกระสุน
     void Shoot()
     {
-        var bullet = Instantiate(bulletPrefab, cameraTransform.position,Quaternion.identity);
+        if (isReloading || currentAmmo <= 0)
+            return;
+
+        var bullet = Instantiate(bulletPrefab, cameraTransform.position, Quaternion.identity);
         var rb = bullet.GetComponent<Rigidbody>();
         rb.AddForce(cameraTransform.forward * bulletSpeed, ForceMode.Impulse);
         Destroy(bullet, bulletLifeTime);
+
+        currentAmmo--;
+    }
+
+    // ฟังก์ชันรีโหลด
+    private IEnumerator Reload()
+    {
+        isReloading = true;
+        Debug.Log("Reloading...");
+
+        yield return new WaitForSeconds(reloadTime);
+
+        currentAmmo = maxAmmo;
+        isReloading = false;
+        Debug.Log("Reload complete!");
     }
 }
